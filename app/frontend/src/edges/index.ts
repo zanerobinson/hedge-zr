3c103013f2e1f4e92f779c19bd7f7dd6de053d2a
Zane Robinson <zane.robinson@gmail.com>
2025-06-18 15:00:45 -0500

    cleanup

--- app/frontend/src/edges/index.ts
+++
@@ -1,6 +0,0 @@
-import type { EdgeTypes } from '@xyflow/react';
-
-
-export const edgeTypes = {
-  // Add your custom edge types here!
-} satisfies EdgeTypes;
--- app/frontend/src/services/types.ts
+++
@@ -1,6 +0,0 @@
-export enum ModelProvider {
-  OPENAI = "OPENAI",
-  ANTHROPIC = "ANTHROPIC",
-  GOOGLE = "GOOGLE",
-  AZURE = "AZURE"
-} 
